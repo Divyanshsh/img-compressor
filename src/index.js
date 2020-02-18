c3faba7e190029b2bca15f25d9a5e8a47f8404cf
Divyansh <18698div@gmail.com>
2020-02-18 11:55:53 +0530

    files added initially

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Resizer from 'react-image-file-resizer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ImageCompressingApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { imgVal: null }
+        this.fileChangedHandler = this.fileChangedHandler.bind(this);
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    fileChangedHandler(event) {
+        var fileInput = false
+        if (event.target.files[0]) {
+            fileInput = true
+        }
+        if (fileInput) {
+            Resizer.imageFileResizer(
+                event.target.files[0],
+                700,
+                700,
+                'JPEG',
+                100,
+                0,
+                uri => {
+                    this.setState({ imgVal: uri });
+                },
+                'base64'
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className="container mt-3">
+                {
+                    this.state.imgVal !== null ?
+                        <div className="alert alert-primary" role="alert">
+                            Image is compressed!
+                        </div>
+                        : null
+                }
+                <input className="mt-3" type="file" onChange={this.fileChangedHandler} />
+                {
+                    this.state.imgVal !== null ? <a href={this.state.imgVal} download>
+                        <button type="button" className="btn btn-primary">
+                            Download
+                       </button>
+                    </a> : null
+                }
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<ImageCompressingApp />, document.getElementById('root'));
